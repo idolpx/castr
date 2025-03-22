4725795b03167766d4c264dcf07bcc90f4cd612d
Jaime Idolpx <jaime@idolpx.com>
2025-03-22 11:25:30 -0500

    Update Utils.h

--- libcastr/utils/include/utils/Utils.h
+++ libcastr/utils/include/utils/Utils.h
@@ -22,6 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <algorithm>
 #include <functional>
 #include <string>
+#include <cstdint>
 #include "json/json.h"
 
 static inline std::string getJsonString( Json::Value obj )
